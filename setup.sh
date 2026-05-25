349f1117ead22293f756ef3d0bfe894bdc094556
RileyCornelius <rileyccornelius@gamil.com>
2026-05-25 18:40:25 -0500

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "$SCRIPT_DIR/install/setup.bash"
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export GZ_SIM_RESOURCE_PATH="$SCRIPT_DIR/src/gazebo_sim/models"
+export CYCLONEDDS_URI="file://$SCRIPT_DIR/src/docker/cyclonedds.xml"
